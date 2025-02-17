3a1f081cdbf076851ba383b172d2755abebc3e85
NikitaUssyukin <nikus0108@gmail.com>
2025-02-17 10:06:18 +0500

    update

--- Lectures/G1/Week06/1_regex_intro/04.py
+++ Lectures/G1/Week06/1_regex_intro/04.py
@@ -1,14 +1,14 @@
 # raw strings
 
 # python_str = '\' # error - backslash here escapes the single quote
-python_str = '\\'
-raw_str = r'\\'
+python_str = 'abc123\\'
+raw_str = r'abc123\\'
 
 print(python_str)
 print(raw_str)
 
-python_str = '\n'
-raw_str = r'\n'
+python_str = 'abc123\n'
+raw_str = r'abc123\n'
 
 print(python_str)
 print(raw_str)
