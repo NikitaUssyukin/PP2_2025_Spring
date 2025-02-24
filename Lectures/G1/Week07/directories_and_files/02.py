50fcef30ad3be7a5f52746fcbd42c8de949f6faf
NikitaUssyukin <nikus0108@gmail.com>
2025-02-24 10:59:33 +0500

    update

---
+++ Lectures/G1/Week07/directories_and_files/02.py
@@ -0,0 +1,10 @@
+import os
+
+path = r'C:\\Users\n.ussyukin\Desktop\PP2_2025_Spring\Lectures\G1\Week07\directories_and_files' # absolute path
+# .. - shorthand for previous directory
+# .  - shorthand for current directory
+
+contents = os.listdir(path)
+
+for element in contents:
+    print(element)
